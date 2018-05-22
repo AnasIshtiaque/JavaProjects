a13e2f7d0c318fa40ca916b5a1a3b241054948a3
Anas Ishtiaque <anas.ishtiaque@outlook.com>
2018-05-22 16:11:15 -0400

    changes made

--- PostalCodeCheck/src/PostalCodeChecker.java
+++ PostalCodeCheck/src/PostalCodeChecker.java
@@ -1,5 +1,15 @@
-public class PostalCodeChecker {
-    public static void main(String[] args){
+import java.util.*;
 
+public class PostalCodeChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter postal code (no spaces allowed): ");
+        String input1 = input.next();
+        if(input1.matches("[A-Z][0-9][A-Z][0-9][A-Z][0-9]")){
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Invalid");
+        }
     }
 }
